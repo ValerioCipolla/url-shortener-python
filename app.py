6ca75db5df1d2cd65a7a84e5b5588a06825e5ba9
valeriocipolla <valeriocipolla92@gmail.com>
2022-04-09 16:16:45 +0100

    created create and read ops

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from this import d
 from flask import Flask, render_template, request
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import string
+import random
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
@@ -21,16 +22,39 @@ class Urls(db.Model):
     self.long = long
     self.short = short
 
+  def __repr__(self):
+    return f"long: {self.long} - short: {self.short} \n"
+
+def shorten_url():
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters, k=3)
+        rand_letters = "".join(rand_letters)
+        short_url = Urls.query.filter_by(short=rand_letters).first()
+        if not short_url:
+            return rand_letters
 
 @app.route("/", methods=["POST", "GET"])
+
 def hello_world():
+  urls = Urls.query.all()
+  print(urls)
   if request.method == "POST":
+    url_received = request.form["url"]
+    found_url = Urls.query.filter_by(long=url_received).first()
+    if found_url:
       return {
-          "long_url": request.form["url"]
-        }
+        "long_url": found_url.long,
+        "short_url": found_url.short
+      }
+    else:
+      short_url = shorten_url()
+      new_url = Urls(url_received, short_url)
+      db.session.add(new_url)
+      db.session.commit()
+      return {
+        "long_url": url_received,
+        "short_url": short_url
+      }
   else:
       return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
